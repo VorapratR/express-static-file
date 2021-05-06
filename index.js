e4fd1f4fe7a76af1a4d0af97c2e86c2ed009300d
VorapratR <vorapratratttana@gmail.com>
2021-05-06 16:49:27 +0700

    [Add] azios call api

--- index.js
+++ index.js
@@ -6,9 +6,31 @@ app.use('/static', express.static('public'))
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
+    getBlockScript()
     writeFileFromAPI()
 });
+function getBlockScript() {
+    const axios = require('axios');
+    axios.get('http://dev-cookie-consent.dosetech.co/api/cookie/1')
+        .then(function (response) {
+            if (response.status == 200) {
+                let result = response.data.detail;
+                let blackListScripts = [];
+                for (let index = 0; index < result.blackListScripts.length; index++) {
+                    blackListScripts.push(new RegExp(result.blackListScripts[index].trim()));
+                }
+                return blackListScripts
+            }
 
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function (error) {
+            console.log(error);
+        });
+
+}
 function writeFileFromAPI() {
     fs = require('fs');
     fs.writeFile('./public/js/app.js', `console.log('write/static/js/app.js');
